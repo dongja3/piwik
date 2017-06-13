9926eb7f1c94fe45dc3fb89b34a4d82812d020a0
pengaa <PENGAA@PENGAA-W7.corp.oocl.com>
2017-06-13 10:35:17 +0800

    oocl_piwik_angular

--- fwk_api/oocl_piwik_config.js
+++ fwk_api/oocl_piwik_config.js
@@ -2,7 +2,7 @@ oocl_piwik_config = {
   piwik_sites: [
     {
       piwik_url: 'http://dongja3-w7/piwik',
-      url: '',//your url here
+      url: 'http://zhangte4-w7:3000',//your url here
       siteId: 3,
       cookieid: 'connect.sid'
     }
