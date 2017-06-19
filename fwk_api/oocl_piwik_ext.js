7036513d1542215bd3d452867d84fe4a57b8b84c
pengaa <PENGAA@PENGAA-W7.corp.oocl.com>
2017-06-19 15:14:41 +0800

    oocl_piwik_ext

--- fwk_api/oocl_piwik_ext.js
+++ fwk_api/oocl_piwik_ext.js
@@ -2,16 +2,17 @@
 * For Single Page EXTJS application should call following api before every ajax request
 * oocl_piwik_tracker.setupContext('search/userids');
 */
-
-Ext.Ajax.on("beforerequest", function(conn, options){
-	var piwik_uuid=oocl_piwik_tracker._createContext();
+Ext.Ajax.on("beforerequest", function (conn, options) {
+	var bfname = oocl_piwik_common._ignoreServicePrefix(options.url);
+	oocl_piwik_tracker.setupContext(bfname);
+	var piwik_uuid = oocl_piwik_tracker._createContext();
 	oocl_piwik_common._setPiwikHeader(piwik_uuid);
 	// oocl_piwik_tracker._clearContext();
 	oocl_piwik_tracker._startTiming(piwik_uuid);
 });
-Ext.Ajax.on("requestcomplete",function(conn, response, options){
+Ext.Ajax.on("requestcomplete", function (conn, response, options) {
 	oocl_piwik_tracker._endTiming(options.headers.piwik_uuid);
 });
-Ext.Ajax.on("requestexception",function(conn, response, options){
+Ext.Ajax.on("requestexception", function (conn, response, options) {
 	oocl_piwik_tracker._endTiming(options.headers.piwik_uuid);
 });
--- fwk_api/test/unit_test.js
+++ fwk_api/test/unit_test.js
@@ -1,92 +1,107 @@
 var piwik_uuid;
-QUnit.test('_isAngular',function(assert){ 
-    if(oocl_piwik_common._isAngular())
-      assert.ok(oocl_piwik_common._isAngular(),'pass');
-    else assert.ok(!oocl_piwik_common._isAngular(),'pass');
+QUnit.test('_isAngular', function (assert) {
+    if (oocl_piwik_common._isAngular())
+        assert.ok(oocl_piwik_common._isAngular(), 'pass');
+    else assert.ok(!oocl_piwik_common._isAngular(), 'pass');
 });
 
-QUnit.test('_isExt',function(assert){ 
-     assert.ok(!oocl_piwik_common._isExt(),'pass');
+QUnit.test('_isExt', function (assert) {
+    assert.ok(oocl_piwik_common._isExt(), 'pass');
 });
 
-QUnit.test('_getDoctitle',function(assert){ 
-    if(!oocl_piwik_common._isAngular())
-      assert.equal(oocl_piwik_common._getDocTitle(),'QUnit Test','pass');
-    else  assert.equal(oocl_piwik_common._getDocTitle(),'','pass');
+QUnit.test('_getDoctitle', function (assert) {
+    if (!oocl_piwik_common._isAngular())
+        assert.equal(oocl_piwik_common._getDocTitle(), 'QUnit Test', 'pass');
+    else assert.equal(oocl_piwik_common._getDocTitle(), '', 'pass');
 });
 
-QUnit.test('_getCustomUrl',function(assert){ 
-    if(!oocl_piwik_common._isAngular())
-      assert.equal(oocl_piwik_common._getCustomUrl(),'unit_test.html','pass');
-    else  assert.equal(oocl_piwik_common._getCustomUrl(),'','pass');
+QUnit.test('_getCustomUrl', function (assert) {
+    if (!oocl_piwik_common._isAngular())
+        assert.equal(oocl_piwik_common._getCustomUrl(), 'unit_test.html', 'pass');
+    else assert.equal(oocl_piwik_common._getCustomUrl(), '', 'pass');
 });
 
-QUnit.test('setupContext',function(assert){ 
-     oocl_piwik_tracker.setupContext('test');
-     assert.ok(oocl_piwik_bfName=='test'&&oocl_piwik_customUrl=='test','pass');
+QUnit.test('setupContext', function (assert) {
+    oocl_piwik_tracker.setupContext('test');
+    assert.ok(oocl_piwik_bfName == 'test' && oocl_piwik_customUrl == 'test', 'pass');
 
 
-     oocl_piwik_tracker.setupContext();
-     assert.ok(oocl_piwik_bfName==null&&oocl_piwik_customUrl==null,'pass');
+    oocl_piwik_tracker.setupContext();
+    assert.ok(oocl_piwik_bfName == null && oocl_piwik_customUrl == null, 'pass');
 });
 
-QUnit.test('_clearContext',function(assert){ 
-     oocl_piwik_tracker._clearContext();
-     assert.ok(oocl_piwik_bfName==null&&oocl_piwik_customUrl==null,'pass');
+QUnit.test('_clearContext', function (assert) {
+    oocl_piwik_tracker._clearContext();
+    assert.ok(oocl_piwik_bfName == null && oocl_piwik_customUrl == null, 'pass');
 
 });
 
-QUnit.test('_createContext',function(assert){ 
-     piwik_uuid=oocl_piwik_tracker._createContext();
-     var expect={
-         "bfName":oocl_piwik_common._getDocTitle(),
-         "customUrl":oocl_piwik_common._getCustomUrl()
-     };
-     var actual=JSON.parse(localStorage.getItem(piwik_uuid))
-     assert.propEqual(actual,expect,'pass');
-});
-
-
-QUnit.test('_startTiming',function(assert){ 
-     var actual=oocl_piwik_tracker._startTiming();
-     assert.strictEqual(actual,undefined,'pass');
-});
-
-
-QUnit.test('_endTiming',function(assert){ 
-     oocl_piwik_tracker._endTiming(piwik_uuid);
-     var actual=localStorage.getItem(piwik_uuid);
-      assert.strictEqual(actual,null,'pass');
-});
-
-QUnit.test('Context',function(assert){ 
-    var actual=new oocl_piwik.Context('testtitle','testurl');
-    var expect={
-        bfName:'testtitle',
-        customUrl:'testurl'
+QUnit.test('_createContext', function (assert) {
+    piwik_uuid = oocl_piwik_tracker._createContext();
+    var expect = {
+        "bfName": oocl_piwik_common._getDocTitle(),
+        "customUrl": oocl_piwik_common._getCustomUrl()
     };
-     assert.propEqual(actual,expect,'pass');
-     if(!oocl_piwik_common._isAngular()){
-         actual=new oocl_piwik.Context();
-         expect={
-             bfName:'QUnit Test',
-             customUrl:'unit_test.html'
+    var actual = JSON.parse(localStorage.getItem(piwik_uuid))
+    assert.propEqual(actual, expect, 'pass');
+});
+
+
+QUnit.test('_startTiming', function (assert) {
+    var actual = oocl_piwik_tracker._startTiming();
+    assert.strictEqual(actual, undefined, 'pass');
+});
+
+
+QUnit.test('_endTiming', function (assert) {
+    oocl_piwik_tracker._endTiming(piwik_uuid);
+    var actual = localStorage.getItem(piwik_uuid);
+    assert.strictEqual(actual, null, 'pass');
+});
+
+QUnit.test('Context', function (assert) {
+    var actual = new oocl_piwik.Context('testtitle', 'testurl');
+    var expect = {
+        bfName: 'testtitle',
+        customUrl: 'testurl'
+    };
+    assert.propEqual(actual, expect, 'pass');
+    if (!oocl_piwik_common._isAngular()) {
+        actual = new oocl_piwik.Context();
+        expect = {
+            bfName: 'QUnit Test',
+            customUrl: 'unit_test.html'
         };
-        assert.propEqual(actual,expect,'pass');
+        assert.propEqual(actual, expect, 'pass');
     }
     else {
-        actual=new oocl_piwik.Context();
-        expect={
-             bfName:'',
-             customUrl:''
-         };
-        assert.propEqual(actual,expect,'pass');
+        actual = new oocl_piwik.Context();
+        expect = {
+            bfName: '',
+            customUrl: ''
+        };
+        assert.propEqual(actual, expect, 'pass');
     }
 });
 
-QUnit.test('_ignoreServicePrefix',function(assert){ 
-     var url='http://localhost/unit_test.html?user=111&password=222';
-     var actual=oocl_piwik_common._ignoreServicePrefix(url);
-     var expect='/unit_test.html';
-     assert.equal(actual,expect,'pass');
+QUnit.test('_ignoreServicePrefix', function (assert) {
+    var url = 'http://localhost/piwiktest/fwk_api/test/unit_test.html?user=111&password=222';
+    var actual = oocl_piwik_common._ignoreServicePrefix(url);
+    var expect = '/piwiktest/fwk_api/test/unit_test.html';
+    assert.equal(actual, expect, 'pass');
+});
+
+QUnit.test('extGetCustomUrl', function (assert) {
+    var actual = '';
+    var done = assert.async();
+    var url = 'http://localhost/piwik/piwik.js?user=111&password=222';
+    var expect = '/piwik/piwik.js';
+    Ext.Ajax.request({
+        url: url,
+        success: function (data, request) {
+            actual = oocl_piwik_common._ignoreServicePrefix(request.url);
+            assert.equal(actual, expect, 'pass');
+            done();
+        }
+    });
 });
